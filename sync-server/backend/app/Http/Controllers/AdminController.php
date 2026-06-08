71bdce4e8813add05e0d57c80c11b753d40b5996
Petar Petkov <petarpetkov@Petars-MacBook-Air-2.local>
2026-06-08 18:40:35 +0400

    fix: add missing Controller import in AdminController

--- sync-server/backend/app/Http/Controllers/AdminController.php
+++ sync-server/backend/app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UsageEvent;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
